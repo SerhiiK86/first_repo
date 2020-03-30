24817189bb6f65239e641d4037f807f12056a3f9
SerhiiK86 <kulichokserj@gmail.com>
2020-03-30 18:57:09 +0300

    'last commit'

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const h1 = document.querySelector('h1');
+const array = [1,2,3]
 
 h1.addEventListener('click',()=>{
-    h1.style.color = 'lightgreen'
+    h1.style.color = 'lightgreen';
+    array.map((item)=>{
+        console.log(item*2)
+    })
 })
+
